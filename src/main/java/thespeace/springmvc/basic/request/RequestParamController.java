bbc40c40b2174a02019f34df9833cec7ea4baa82
thespeace <thespeace1@gmail.com>
2024-03-08 18:37:22 +0900

    FEAT: HTTP Request Parameters - @RequestParam

--- src/main/java/thespeace/springmvc/basic/request/RequestParamController.java
+++ src/main/java/thespeace/springmvc/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * <h2>HTTP 요청 파라미터 - 쿼리 파라미터, HTML Form</h2>
@@ -35,6 +38,7 @@ public class RequestParamController {
 
     /**
      * 반환 타입이 없으면서 이렇게 응답에 값을 직접 집어넣으면, view 조회X
+     * @see <a href="http://localhost:8080/request-param-v1?username=hello&age=20">test url</a>
      * @see <a href="http://localhost:8080/basic/hello-form.html">HTML form test url</a>
      */
     @RequestMapping("/request-param-v1")
@@ -44,5 +48,97 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    /**
+     * @param memberName @RequestParam 사용
+     * @param memberAge 파라미터 이름으로 바인딩
+     * @see <a href="http://localhost:8080/request-param-v2?username=hello&age=20">test url</a>
+     */
+    @ResponseBody //View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력 == @RestController와 같은 효과. 클래스단위 -> 메서드단위.
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+
+        return "ok";
+    }
+
+    /**
+     * @param username @RequestParam 사용
+     * @param age HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     * @see <a href="http://localhost:8080/request-param-v3?username=hello&age=20">test url</a>
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @param username @RequestParam 사용
+     * @param age String, int, Integer 등의 단순 타입이면 @RequestParam 도 생략 가능
+     * @see <a href="http://localhost:8080/request-param-v4?username=hello&age=20">test url</a>
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * <h2>파라미터 필수 여부 - requestParamRequired</h2>
+     * @param username 필수(default), 빈문자 입력받을 수 있다. parameter가 없으면 400 예외가 발생
+     * @param age int age -> null을 int에 입력하는 것은 불가능(500 예외 발생), 따라서 Integer 변경해야 함(또는 defaultValue 사용)
+     *
+     * @see <a href="http://localhost:8080/request-param-required?username=hello&age=20">test url</a>
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+
+    /**
+     * <h2>기본 값 적용 - requestParamDefault</h2>
+     * @param username defaultValue 사용
+     * @param age defaultValue는 빈 문자의 경우에도 적용
+     *
+     * @see <a href="http://localhost:8080/request-param-default?username=hello&age=20">test url</a>
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * <h2>파라미터를 Map으로 조회하기 - requestParamMap</h2>
+     * @param paramMap 파라미터의 값이 1개가 확실하다면 Map 을 사용해도 되지만, 그렇지 않다면 MultiValueMap 을 사용하자.
+     * <ul>
+     *     <li>Map : Map(key=value)</li>
+     *     <li>MultiValueMap : MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])</li>
+     * </ul>
+     *
+     * @see <a href="http://localhost:8080/request-param-map?username=hello&age=20">test url</a>
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"),
+                paramMap.get("age"));
+        return "ok";
     }
 }
